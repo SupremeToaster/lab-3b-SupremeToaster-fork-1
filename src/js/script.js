75e41405f0eb0a31c9f5532e908c59681339fe1e
Tao Yang <taoyang.peter@gmail.com>
2023-09-02 10:23:10 -0600

    index script

--- src/js/script.js
+++ src/js/script.js
@@ -32,37 +32,19 @@ $(document).ready(function () {
     );
   }
 
-  // Event handler for the sort checkbox
-  $("#cb-sort").change(function () {
-    // Perform AJAX request to sort the tasks
-    $.post(
-      "actions/sort_action.php",
-      { sort: this.checked },
-      function (data) {
-        // Update the UI based on the response
-        if (data.success) {
-          // Sort the tasks in the UI
-          // (This part can be customized based on how you want to sort the tasks)
-        }
-      },
-      "json"
-    );
-  });
-
-  // Event handler for the filter checkbox
-  $("#cb-filter").change(function () {
-    // Perform AJAX request to filter the tasks
-    $.post(
-      "actions/filter_action.php",
-      { filter: this.checked },
-      function (data) {
-        // Update the UI based on the response
-        if (data.success) {
-          // Filter the tasks in the UI
-          // (This part can be customized based on how you want to filter the tasks)
-        }
-      },
-      "json"
-    );
-  });
+// Event handler for the sort checkbox
+$("#cb-sort").change(function () {
+  // Perform AJAX request to sort the tasks
+  $.post(
+    "actions/sort_action.php",
+    { sort: this.checked },
+    function (data) {
+      // Update the UI based on the response
+      if (data.success) {
+        // Sort the tasks in the UI
+        // (This part can be customized based on how you want to sort the tasks)
+      }
+    },
+    "json"
+  );
 });
